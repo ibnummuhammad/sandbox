bbd042fce758ec2008f6836c5c94ce73af1e23a8
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2023-12-15 15:21:48 +0700

    Add flag

--- go/project-pertama/src/backend/main.go
+++ go/project-pertama/src/backend/main.go
@@ -1,18 +1,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
 )
 
 func main() {
-	var argsRaw = os.Args
-	fmt.Println(argsRaw)
-	fmt.Printf("-> %#v\n", argsRaw)
-	// -> []string{".../bab45", "banana", "potato", "ice cream"}
+	var name = flag.String("name", "anonymous", "type your name")
+	var age = flag.Int64("age", 25, "type your age")
 
-	var args = argsRaw[1:]
-	fmt.Println(args)
-	fmt.Printf("-> %#v\n", args)
-	// -> []string{"banana", "potatao", "ice cream"}
+	flag.Parse()
+	fmt.Printf("name\t: %s\n", *name)
+	fmt.Printf("age\t: %d\n", *age)
 }
