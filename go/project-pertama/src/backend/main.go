18c55843d7cc6822037b43bf94a16775dca85cc1
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2023-12-15 15:20:48 +0700

    Print argsRaw

--- go/project-pertama/src/backend/main.go
+++ go/project-pertama/src/backend/main.go
@@ -7,10 +7,12 @@ import (
 
 func main() {
 	var argsRaw = os.Args
+	fmt.Println(argsRaw)
 	fmt.Printf("-> %#v\n", argsRaw)
 	// -> []string{".../bab45", "banana", "potato", "ice cream"}
 
 	var args = argsRaw[1:]
+	fmt.Println(args)
 	fmt.Printf("-> %#v\n", args)
 	// -> []string{"banana", "potatao", "ice cream"}
 }
