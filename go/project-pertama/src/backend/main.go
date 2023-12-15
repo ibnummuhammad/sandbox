66acc38ddc5b636e616eaa547ba963951bd2d64f
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2023-12-15 16:07:01 +0700

    Add flag

--- go/project-pertama/src/backend/main.go
+++ go/project-pertama/src/backend/main.go
@@ -8,8 +8,11 @@ import (
 func main() {
 	var name = flag.String("name", "anonymous", "type your name")
 	var age = flag.Int64("age", 25, "type your age")
+	var data2 string
+	flag.StringVar(&data2, "gender", "male", "type your gender")
 
 	flag.Parse()
 	fmt.Printf("name\t: %s\n", *name)
 	fmt.Printf("age\t: %d\n", *age)
+	fmt.Println(data2)
 }
