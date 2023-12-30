9d33c0baf841dc2008693c508692748309402fb0
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2023-12-30 20:47:35 +0700

    Add python/

---
+++ cs/hello.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace HelloWorldApp
+{
+    class Geeks
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Helloowei");
+
+            Console.ReadKey();
+        }
+    }
+}
