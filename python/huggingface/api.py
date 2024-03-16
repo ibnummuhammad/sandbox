d17607a8c741407a6ba652241d0a0450d7d9cf8f
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2024-03-16 16:44:49 +0700

    Add huggingface api.py

--- python/huggingface/api.py
+++ python/huggingface/api.py
@@ -1,5 +1,7 @@
+import os
 import requests
 
+API_TOKEN = os.getenv("API_TOKEN")
 API_URL = "https://api-inference.huggingface.co/models/mistralai/Mixtral-8x7B-Instruct-v0.1"
 headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
@@ -14,3 +16,4 @@ output = query(
         "inputs": "Can you please let us know more details about your ",
     }
 )
+print(output)
