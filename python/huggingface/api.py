5adb7841a9bcf34fb59175f0192b80e3a96eb349
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2024-03-16 16:42:02 +0700

    Add huggingface api.py

---
+++ python/huggingface/api.py
@@ -0,0 +1,16 @@
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/mistralai/Mixtral-8x7B-Instruct-v0.1"
+headers = {"Authorization": f"Bearer {API_TOKEN}"}
+
+
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+
+
+output = query(
+    {
+        "inputs": "Can you please let us know more details about your ",
+    }
+)
