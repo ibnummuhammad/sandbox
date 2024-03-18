763c5e2af13f5353d9cb95ef32edb699530e9294
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2024-03-18 12:47:46 +0700

    Add LLMChain()

--- python/huggingface/langchain_prompt.py
+++ python/huggingface/langchain_prompt.py
@@ -1,6 +1,10 @@
+import os
+
 from langchain_community.llms import HuggingFaceEndpoint
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
+
+HUGGINGFACEHUB_API_TOKEN = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 
 question = "Who won the FIFA World Cup in the year 1994? "
 
@@ -9,4 +13,12 @@ template = """Question: {question}
 Answer: Let's think step by step."""
 
 prompt = PromptTemplate.from_template(template)
-print(prompt)
+
+repo_id = "mistralai/Mistral-7B-Instruct-v0.2"
+
+llm = HuggingFaceEndpoint(
+    repo_id=repo_id, max_length=128, temperature=0.5, token=HUGGINGFACEHUB_API_TOKEN
+)
+llm_chain = LLMChain(prompt=prompt, llm=llm)
+print("disini")
+print(llm_chain.run(question))
