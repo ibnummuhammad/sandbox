8f40eeeb5649d4d8f184864994158303ec0cbd1c
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2024-03-18 11:52:11 +0700

    Add PromptTemplate()

--- python/huggingface/langchain.py
+++ python/huggingface/langchain.py
@@ -1,4 +1,9 @@
 from huggingface_hub import InferenceClient
+from langchain_core.prompts import PromptTemplate
 
 client = InferenceClient("mistralai/Mixtral-8x7B-Instruct-v0.1")
-print(client)
+
+template = "<s>[INST] tell me a joke [/INST]"
+
+prompt = PromptTemplate.from_template(template)
+print(prompt)
