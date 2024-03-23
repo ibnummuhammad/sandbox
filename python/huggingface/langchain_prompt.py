c37e8ef87f1e3dedd7ad1fc6ad119071ebf8a951
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2024-03-23 21:12:37 +0700

    Set repo_id to mistralai/Mixtral-8x7B-Instruct-v0.1

--- python/huggingface/langchain_prompt.py
+++ python/huggingface/langchain_prompt.py
@@ -14,7 +14,7 @@ Answer: Let's think step by step."""
 
 prompt = PromptTemplate.from_template(template)
 
-repo_id = "mistralai/Mistral-7B-Instruct-v0.2"
+repo_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"
 
 llm = HuggingFaceEndpoint(
     repo_id=repo_id, max_length=128, temperature=0.5, token=HUGGINGFACEHUB_API_TOKEN
