67911fe495aef3f5a401da4b57b3ef2d810d82c2
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2024-03-23 21:20:58 +0700

    Fix llm_response

--- python/huggingface/langchain_prompt.py
+++ python/huggingface/langchain_prompt.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 from langchain_community.llms.huggingface_endpoint import HuggingFaceEndpoint
@@ -18,4 +19,5 @@ repo_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"
 
 llm = HuggingFaceEndpoint(repo_id=repo_id, temperature=0.5)
 llm_chain = LLMChain(prompt=prompt, llm=llm)
-print(llm_chain.invoke(question))
+llm_response = llm_chain.invoke(question)
+print(json.dumps(llm_response, indent=2))
