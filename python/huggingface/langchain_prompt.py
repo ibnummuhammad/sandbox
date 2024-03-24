e9979c493e2d55b1c7dfcd8773322ce71ba36b53
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2024-03-24 18:48:11 +0700

    Prompt for sentiment

--- python/huggingface/langchain_prompt.py
+++ python/huggingface/langchain_prompt.py
@@ -5,10 +5,13 @@ from langchain_community.llms.huggingface_endpoint import HuggingFaceEndpoint
 from langchain.chains.llm import LLMChain
 from langchain.prompts import PromptTemplate
 
-question = "Who won the FIFA World Cup in the year 1994?"
+input = "I think the food was okay."
 
-template = """Q: {question}?
-A: """
+template = """Classify the text into neutral, negative, or positive
+
+Text: {input}
+
+Sentiment:"""
 
 prompt = PromptTemplate.from_template(template)
 
@@ -16,5 +19,5 @@ repo_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"
 
 llm = HuggingFaceEndpoint(repo_id=repo_id, temperature=0.5)
 llm_chain = LLMChain(prompt=prompt, llm=llm)
-llm_response = llm_chain.invoke(question)
+llm_response = llm_chain.invoke(input)
 print(json.dumps(llm_response, indent=2))
