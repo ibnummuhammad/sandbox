6b98d11fe8d42b53f67151597592ce0cc4ab34cd
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2024-03-24 19:34:24 +0700

    Prompt 'question answering'

--- python/huggingface/langchain_prompt.py
+++ python/huggingface/langchain_prompt.py
@@ -4,11 +4,15 @@ from langchain_community.llms.huggingface_endpoint import HuggingFaceEndpoint
 from langchain.chains.llm import LLMChain
 from langchain.prompts import PromptTemplate
 
-input = "Author-contribution statements and acknowledgements in research papers should state clearly and specifically whether, and to what extent, the authors used AI technologies such as ChatGPT in the preparation of their manuscript and analysis. They should also indicate which LLMs were used. This will alert editors and reviewers to scrutinize manuscripts more carefully for potential biases, inaccuracies and improper source crediting. Likewise, scientific journals should be transparent about their use of LLMs, for example when selecting submitted manuscripts."
+input = "What was OKT3 originally sourced from?"
 
-template = """{input}
+template = """Answer the question based on the context below. Keep the answer short and concise. Respond "Unsure about answer" if not sure about the answer.
 
-Mention the large language model based product mentioned in the paragraph above:"""
+Context: Teplizumab traces its roots to a New Jersey drug company called Ortho Pharmaceutical. There, scientists generated an early version of the antibody, dubbed OKT3. Originally sourced from mice, the molecule was able to bind to the surface of T cells and limit their cell-killing potential. In 1986, it was approved to help prevent organ rejection after kidney transplants, making it the first therapeutic antibody allowed for human use.
+
+Question: {input}
+
+Answer:"""
 
 prompt = PromptTemplate.from_template(template)
 
