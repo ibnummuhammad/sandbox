130d8bc1c830f5769d17d5f39087ddc90e0ff9c8
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2024-03-24 19:31:37 +0700

    Prompt 'information extraction'

--- python/huggingface/langchain_prompt.py
+++ python/huggingface/langchain_prompt.py
@@ -4,11 +4,11 @@ from langchain_community.llms.huggingface_endpoint import HuggingFaceEndpoint
 from langchain.chains.llm import LLMChain
 from langchain.prompts import PromptTemplate
 
-input = "Explain antibiotics"
+input = "Author-contribution statements and acknowledgements in research papers should state clearly and specifically whether, and to what extent, the authors used AI technologies such as ChatGPT in the preparation of their manuscript and analysis. They should also indicate which LLMs were used. This will alert editors and reviewers to scrutinize manuscripts more carefully for potential biases, inaccuracies and improper source crediting. Likewise, scientific journals should be transparent about their use of LLMs, for example when selecting submitted manuscripts."
 
 template = """{input}
 
-A: """
+Mention the large language model based product mentioned in the paragraph above:"""
 
 prompt = PromptTemplate.from_template(template)
 
