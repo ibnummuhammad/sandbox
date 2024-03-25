f84c26a6e30e05e548696bf47c03154ac52c2c3f
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2024-03-25 14:34:49 +0700

    Add df_to_parquet.py

--- python/df_to_parquet.py
+++ python/df_to_parquet.py
@@ -1,7 +1,20 @@
 import pandas as pd
 
 
-df = pd.DataFrame(data={'col1': [1, 2], 'col2': [3, 4]})
-df.to_parquet('df.parquet.gzip', compression='gzip')
-df_output = pd.read_parquet('df.parquet.gzip')
+df = pd.DataFrame(data={"col1": [1, 2], "col2": [3, 4]})
+print(df)
+print(df.dtypes)
+
+df = df.astype(
+    {
+        "col1": str,
+        "col2": str,
+    }
+)
+print(df)
+print(df.dtypes)
+
+df.to_parquet("df.parquet.gzip", compression="gzip")
+df_output = pd.read_parquet("df.parquet.gzip")
 print(df_output)
+print(df_output.dtypes)
