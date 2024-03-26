53e36435ed15b19f92a5b335863bf93eeffc11b0
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2024-03-26 14:43:39 +0700

    Add prompt_mistral.py

--- python/huggingface/prompt_mistral.py
+++ python/huggingface/prompt_mistral.py
@@ -5,4 +5,10 @@ import pandas as pd
 
 
 ktp_ocr_data = pd.read_csv("~/Downloads/ktp_ocr_data_20240319 (3).csv")
-print(ktp_ocr_data)
+ktp_ocr_data = ktp_ocr_data[["user_id", "ocr_text"]]
+print(ktp_ocr_data.count())
+
+ktp_ocr_data_1 = ktp_ocr_data.iloc[:1000].reset_index(drop=True)
+ktp_ocr_data_2 = ktp_ocr_data.iloc[1000:2000].reset_index(drop=True)
+ktp_ocr_data_3 = ktp_ocr_data.iloc[2000:3000].reset_index(drop=True)
+ktp_ocr_data_4 = ktp_ocr_data.iloc[3000:].reset_index(drop=True)
