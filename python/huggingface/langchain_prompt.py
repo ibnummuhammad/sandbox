918d1558397c3bef76a5e865994602bb66cf882f
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2024-03-27 11:40:14 +0700

    Revert input

--- python/huggingface/langchain_prompt.py
+++ python/huggingface/langchain_prompt.py
@@ -4,7 +4,7 @@ from langchain_community.llms.huggingface_endpoint import HuggingFaceEndpoint
 from langchain.chains.llm import LLMChain
 from langchain.prompts import PromptTemplate
 
-input = "PROVINSIDKIJAKARTA JAKARTA BARAT IK 3172011411890004 ama FIRMAN GUNAWAN empat/Tgll Lahir JAKARTA, 14-11-1989 nis kelamin LAKI-LAKI Gol. Darah A amat PERMATA TAMAN PALEM BLOK D-2/19 RT/RW 005/003 Kel/Desa PEGADUNGAN Kecamatan : KALIDERES jama : KRISTEN atus Perkawinan: BELUM KAWIN JAKARTA BARAT kerjaan : KARYAWAN SWASTA 12-02-2016 warganegaraan: WNI rlaku Hingga : SEUMUR HIDUP fue"
+input = "PROVINSI DKI JAKARTA JAKARTA SELATAN IK 3174073007960007 na : DIMAS RADITYO npat/Tgl Lahir : JAKARTA, 30-07-1996 is kelamin : LAKI-LAKI Gol. Darah :0 mat : JL.RADIO IV NO.2 RT/RW 001/004 Kel/Desa : KRAMAT PELA Kecamatan : KEBAYORAN BARU ma : ISLAM us Perkawinan: KAWIN erjaan : KARYAWAN SWASTA JAKARTA SELATAN arganegaraan: WNI 27-12-2021 aku Hingga : SEUMUR HIDUP"
 
 template = """### INSTRUCTIONS ###
 I have a text data that I got from doing an OCR to Indonesian ID Card (KTP or Kartu Tanda Penduduk). You must convert the text into a structured format. The structured output must be a markdown code snippet formatted in the following schema, including the leading and trailing "```json" and "```":
