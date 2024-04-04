e90a47f69382d215e2c4a60f1c81f34ebc4e787b
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2024-04-04 21:55:37 +0700

    Add go/check_type.go

--- go/check_type.go
+++ go/check_type.go
@@ -1,22 +1,22 @@
+// Golang program to find the variable type
 package main
 
+// importing required packages
 import (
 	"fmt"
-	"reflect"
 )
 
+// main function
 func main() {
-	t1 := "text"
-	t2 := []string{"apple", "strawberry", "blueberry"}
-	t3 := map[string]float64{"strawberry": 3.2, "blueberry": 1.2}
-	t4 := 2
-	t5 := 4.5
-	t6 := true
+	f := true
+	st := ""
+	a := 1
+	d := 1.0
+	arr := []string{"Go", "Is", "Fun"}
 
-	fmt.Printf("t1: %s\n", reflect.TypeOf(t1))
-	fmt.Printf("t2: %s\n", reflect.TypeOf(t2))
-	fmt.Printf("t3: %s\n", reflect.TypeOf(t3))
-	fmt.Printf("t4: %s\n", reflect.TypeOf(t4))
-	fmt.Printf("t5: %s\n", reflect.TypeOf(t5))
-	fmt.Printf("t6: %s\n", reflect.TypeOf(t6))
+	fmt.Printf("%T\n", f)
+	fmt.Printf("%T\n", st)
+	fmt.Printf("%T\n", a)
+	fmt.Printf("%T\n", d)
+	fmt.Printf("%T\n", arr)
 }
