ca763d3d69f558f008bf0d4862945d7f614c5cd4
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2024-04-05 00:25:04 +0700

    Add go/go_pointer/golang_pointer.go

--- go/go_pointer/golang_pointer.go
+++ go/go_pointer/golang_pointer.go
@@ -5,7 +5,9 @@ import "fmt"
 func main() {
 	i, j := 42, 2701
 
-	p := &i         // point to i
+	p := &i        // point to i
+	fmt.Println(p) // read i through the pointer
+	fmt.Printf("%T\n", p)
 	fmt.Println(*p) // read i through the pointer
 	*p = 21         // set i through the pointer
 	fmt.Println(i)  // see the new value of i
