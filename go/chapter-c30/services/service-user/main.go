061d253e78122b613742c6cddca2f368981a09d6
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2024-04-16 14:47:18 +0700

    Add services/service-user/main.go

---
+++ go/chapter-c30/services/service-user/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"chapter-c30/common/config"
+	"chapter-c30/common/model"
+
+	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/grpc"
+)
