adb52a3b9cc9d14ac63fecf80cacbdbfe588b062
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2024-04-16 13:16:07 +0700

    Add main.go

--- protobuf/chapter-c29/main.go
+++ protobuf/chapter-c29/main.go
@@ -2,43 +2,45 @@ package main
 
 import (
 	"fmt"
-	"os"
+	// "os"
 
 	"chapter-c29/model"
 )
 
 func main() {
 	var user1 = &model.User{
-		id: "u001",
+		Id: "u001",
 		Name: "Sylvana Windrunner",
 		Password: "f0r Th3 H0rd3",
 		Gender: model.UserGender_FEMALE,
 	}
 
-	var userList = &model.UserList{
-		List: []*model.User{
-			user1,
-		},
-	}
+	// var userList = &model.UserList{
+	// 	List: []*model.User{
+	// 		user1,
+	// 	},
+	// }
 
-	var garage1 = &model.Garage{
-		Id: "g001",
-		Name: "Kalimdor",
-		Coordinate: &model.GarageCoordinate{
-			Latitude: 23.2212847,
-			Longitude: 53.22033123,
-		},
-	}
+	// var garage1 = &model.Garage{
+	// 	Id: "g001",
+	// 	Name: "Kalimdor",
+	// 	Coordinate: &model.GarageCoordinate{
+	// 		Latitude: 23.2212847,
+	// 		Longitude: 53.22033123,
+	// 	},
+	// }
 
-	var garageList = &model.GarageList{
-		List: []*model.Garage{
-			garage1,
-		},
-	}
+	// var garageList = &model.GarageList{
+	// 	List: []*model.Garage{
+	// 		garage1,
+	// 	},
+	// }
 
-	var garageListByUser = &model.GarageListByUser{
-		List: map[string]*model.GarageList{
-			user1.Id: garageList,
-		},
-	}
+	// var garageListByUser = &model.GarageListByUser{
+	// 	List: map[string]*model.GarageList{
+	// 		user1.Id: garageList,
+	// 	},
+	// }
+
+	fmt.Printf("# ==== Original\n       %#v \n", user1)
 }
