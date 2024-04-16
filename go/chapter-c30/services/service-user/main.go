a7fe3c4d009bd5167101cd58daa7c94281e9a5e0
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2024-04-16 14:55:59 +0700

    Add services/service-user/main.go

--- go/chapter-c30/services/service-user/main.go
+++ go/chapter-c30/services/service-user/main.go
@@ -11,3 +11,10 @@ import (
 	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/grpc"
 )
+
+var localStorage * model.UserList
+
+func init() {
+	localStorage = new(model.UserList)
+	localStorage.List = make([]*model.User, 0)
+}
