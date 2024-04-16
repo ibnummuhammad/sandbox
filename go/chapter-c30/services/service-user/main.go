9bdadb53e3569ebf002e5b4b9f4f36c2a640d545
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2024-04-16 14:59:39 +0700

    Add services/service-user/main.go

--- go/chapter-c30/services/service-user/main.go
+++ go/chapter-c30/services/service-user/main.go
@@ -18,3 +18,21 @@ func init() {
 	localStorage = new(model.UserList)
 	localStorage.List = make([]*model.User, 0)
 }
+
+type UsersServer struct {
+	model.UnimplementedUsersServer
+}
+
+func (UsersServer) Register(_ context.Context, param *model.User) (*emptypb.Empty, error) {
+	user := param
+
+	localStorage.List = append(localStorage.List, user)
+
+	log.Println("Registring user", user.String())
+
+	return new(emptypb.Empty), nil
+}
+
+func (UsersServer) List(context.Context, *emptypb.Empty) (*model.UserList, error) {
+	return localStorage, nil
+}
