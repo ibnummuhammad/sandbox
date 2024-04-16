c28c39ad09ea269582458d0a2022fa42297489ba
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2024-04-16 16:01:09 +0700

    Add client/main.go

--- go/chapter-c30/client/main.go
+++ go/chapter-c30/client/main.go
@@ -31,3 +31,21 @@ func serviceUser() model.UsersClient {
 
 	return model.NewUsersClient(conn)
 }
+
+func main() {
+	user1 := model.User{
+		Id: "n001",
+		Name: "Noval Agung",
+		Password: "kw8d hl12/3m,a",
+		Gender: model.UserGender(model.UserGender_value["MALE"]),
+	}
+
+	garage1 := model.Garage{
+		Id: "q001",
+		Name: "Quel'thalas",
+		Coordinate: &model.GarageCoordinate{
+			Latitude: 45.123123123,
+			Longitude: 54.1231313123,
+		}
+	}
+}
