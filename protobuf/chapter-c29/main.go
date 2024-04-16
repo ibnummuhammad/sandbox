8704d142a09391f93da1e1b53734b6b6e1f0548f
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2024-04-16 13:03:22 +0700

    Add main.go

---
+++ protobuf/chapter-c29/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"chapter-c29/model"
+)
+
+func main() {
+	var user1 = &model.User{
+		id: "u001",
+		Name: "Sylvana Windrunner",
+		Password: "f0r Th3 H0rd3",
+		Gender: model.UserGender_FEMALE,
+	}
+}
