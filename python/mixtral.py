1a01248db6ea30ddce2631c5ee0f0db61d573b73
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2024-04-16 12:54:32 +0700

    Add mixtral.py

--- python/mixtral.py
+++ python/mixtral.py
@@ -1,1 +1,7 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
+
+
+model_id = "mistralai/Mixtral-8x7B-v0.1"
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+model = AutoModelForCausalLM.from_pretrained(model_id, cache_dir="")
