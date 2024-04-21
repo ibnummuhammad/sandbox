74fa6fe49966b50c2280fb29ce24732e6b52175f
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2024-04-21 21:41:43 +0700

    Format cpp/hello_world.cpp

--- cpp/hello_world.cpp
+++ cpp/hello_world.cpp
@@ -4,16 +4,15 @@
 
 using namespace std;
 
-int main()
-{
-    cout << "Welcome To Geeks For Geeks For Geeks";
- 
-    // Given two numbers
-    int a = 13, b = 22;
- 
-    // Function declared in header
-    // file to find the sum
-    cout << "\nSum is: " << sumOfTwoNumbers(a, b) << endl;
+int main() {
+  cout << "Welcome To Geeks For Geeks For Geeks";
 
-    return 0;
+  // Given two numbers
+  int a = 13, b = 22;
+
+  // Function declared in header
+  // file to find the sum
+  cout << "\nSum is: " << sumOfTwoNumbers(a, b) << endl;
+
+  return 0;
 }
