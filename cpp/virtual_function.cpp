b7cfbd5c0e8e963976161c54cfb46c35539d4013
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2024-04-21 21:49:50 +0700

    Add cpp/virtual_function.cpp

--- cpp/virtual_function.cpp
+++ cpp/virtual_function.cpp
@@ -1,23 +1,28 @@
-#include <iostream>    
-{    
- public:    
- virtual void display()    
- {    
-  cout << "Base class is invoked"<<endl;    
- }    
-};    
-class B:public A    
-{    
- public:    
- void display()    
- {    
-  cout << "Derived Class is invoked"<<endl;    
- }    
-};    
-int main()    
-{    
- A* a;    //pointer of base class    
- B b;     //object of derived class    
- a = &b;    
- a->display();   //Late Binding occurs    
-}    
+#include <iostream>  
+using namespace std;  
+class A  
+{  
+   int x=5;  
+    public:  
+    void display()  
+    {  
+        std::cout << "Value of x is : " << x<<std::endl;  
+    }  
+};  
+class B: public A  
+{  
+    int y = 10;  
+    public:  
+    void display()  
+    {  
+        std::cout << "Value of y is : " <<y<< std::endl;  
+    }  
+};  
+int main()  
+{  
+    A *a;  
+    B b;  
+    a = &b;  
+   a->display();  
+    return 0;  
+}  
