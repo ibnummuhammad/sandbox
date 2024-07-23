6cc998b4a8803754d77f97b25918b7a8483d8938
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2024-07-23 14:33:12 +0700

    Add typescript grafana query

--- typescript/helo_react.tsx
+++ typescript/helo_react.tsx
@@ -1,4 +1,36 @@
-import { useState } from "react";
+let podNameSpace: string = "kubeflow";
+let podName: string = "pipeline-4vqqd-3932826485";
+let getCreateTimeSinceEpoch: number = 1721149200000;
+let getLastUpdateTimeSinceEpoch: number = 1721408399000;
 
-let message: string = 'Hello, tipesckir!';
-console.log(message);
+let url: string = "https://grafana.mon.tipnet.xyz/explore";
+let datasource: string = "loki-core-staging-79yu";
+let expr: string = `{namespace=\\"${podNameSpace}\\",pod=\\"${podName}\\"}|=\`\``;
+
+let panesString: string = `
+  {
+    "ghd": {
+      "datasource": "${datasource}",
+      "queries": [
+        {
+          "refId": "A",
+          "expr": "${expr}",
+          "queryType": "range",
+          "datasource": { "type": "loki", "uid": "${datasource}" },
+          "editorMode": "builder"
+        }
+      ],
+      "range": { "from": "${getCreateTimeSinceEpoch}", "to": "${getLastUpdateTimeSinceEpoch}" }
+    }
+  }
+`;
+let panesJson: JSON = JSON.parse(panesString);
+let panes: string = JSON.stringify(panesJson);
+console.log(panes)
+
+const grafanaUrl = new URL(url);
+grafanaUrl.searchParams.append("schemaVersion", "1");
+grafanaUrl.searchParams.append("panes", panes);
+grafanaUrl.searchParams.append("orgId", "1");
+console.log(grafanaUrl);
+console.log(grafanaUrl.toString());
