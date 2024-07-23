a14af242df3edb89aea7a57dd668c9854334207c
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2024-07-23 15:30:31 +0700

    Add typescript grafana query

--- typescript/helo_react.tsx
+++ typescript/helo_react.tsx
@@ -3,30 +3,30 @@ let podName: string = "pipeline-4vqqd-3932826485";
 let getCreateTimeSinceEpoch: number = 1721149200000;
 let getLastUpdateTimeSinceEpoch: number = 1721408399000;
 
-var date = new Date(getCreateTimeSinceEpoch);
+var date: Date = new Date(getCreateTimeSinceEpoch);
 console.log(date);
-var hours = date.getHours();
+const hours = date.getHours();
 console.log(hours);
-var minutes = date.getMinutes();
+const minutes = date.getMinutes();
 console.log(minutes);
-var day = date.getDate();
+const day = date.getDate();
 console.log(day);
-var time = date.getTime();
+const time = date.getTime();
 console.log(time);
 
 var date_kurang = new Date(getCreateTimeSinceEpoch);
 date_kurang.setHours(date_kurang.getHours() - 2);
-var hours1 = date_kurang.getHours();
+const hours1 = date_kurang.getHours();
 console.log(hours1);
-var day1 = date_kurang.getDate();
+const day1 = date_kurang.getDate();
 console.log(day1);
 
 console.log("lebih");
 var date_lebih = new Date(getCreateTimeSinceEpoch);
 date_lebih.setHours(date_lebih.getHours() + 2);
-var hours2 = date_lebih.getHours();
+const hours2 = date_lebih.getHours();
 console.log(hours2);
-var day2 = date_lebih.getDate();
+const day2 = date_lebih.getDate();
 console.log(day2);
 
 // let url: string = "https://grafana.mon.tipnet.xyz/explore";
