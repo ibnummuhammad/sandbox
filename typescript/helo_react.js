d052cf37a7b935defde7a9931e6cbf738cc7747d
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2024-07-23 14:55:59 +0700

    Add typescript grafana query

--- typescript/helo_react.js
+++ typescript/helo_react.js
@@ -2,16 +2,55 @@ var podNameSpace = "kubeflow";
 var podName = "pipeline-4vqqd-3932826485";
 var getCreateTimeSinceEpoch = 1721149200000;
 var getLastUpdateTimeSinceEpoch = 1721408399000;
-var url = "https://grafana.mon.tipnet.xyz/explore";
-var datasource = "loki-core-staging-79yu";
-var expr = "{namespace=\\\"".concat(podNameSpace, "\\\",pod=\\\"").concat(podName, "\\\"}|=``");
-var panesString = "\n  {\n    \"ghd\": {\n      \"datasource\": \"".concat(datasource, "\",\n      \"queries\": [\n        {\n          \"refId\": \"A\",\n          \"expr\": \"").concat(expr, "\",\n          \"queryType\": \"range\",\n          \"datasource\": { \"type\": \"loki\", \"uid\": \"").concat(datasource, "\" },\n          \"editorMode\": \"builder\"\n        }\n      ],\n      \"range\": { \"from\": \"").concat(getCreateTimeSinceEpoch, "\", \"to\": \"").concat(getLastUpdateTimeSinceEpoch, "\" }\n    }\n  }\n");
-var panesJson = JSON.parse(panesString);
-var panes = JSON.stringify(panesJson);
-console.log(panes);
-var grafanaUrl = new URL(url);
-grafanaUrl.searchParams.append("schemaVersion", "1");
-grafanaUrl.searchParams.append("panes", panes);
-grafanaUrl.searchParams.append("orgId", "1");
-console.log(grafanaUrl);
-console.log(grafanaUrl.toString());
+var date = new Date(getCreateTimeSinceEpoch);
+console.log(date);
+var hours = date.getHours();
+console.log(hours);
+var minutes = date.getMinutes();
+console.log(minutes);
+var day = date.getDate();
+console.log(day);
+var time = date.getTime();
+console.log(time);
+var date_kurang = new Date(getCreateTimeSinceEpoch);
+date_kurang.setHours(date_kurang.getHours() - 2);
+var hours1 = date_kurang.getHours();
+console.log(hours1);
+var day1 = date_kurang.getDate();
+console.log(day1);
+console.log("lebih");
+var date_lebih = new Date(getCreateTimeSinceEpoch);
+date_lebih.setHours(date_lebih.getHours() + 2);
+var hours2 = date_lebih.getHours();
+console.log(hours2);
+var day2 = date_lebih.getDate();
+console.log(day2);
+// let url: string = "https://grafana.mon.tipnet.xyz/explore";
+// let datasource: string = "loki-core-staging-79yu";
+// let expr: string = `{namespace=\\"${podNameSpace}\\",pod=\\"${podName}\\"}|=\`\``;
+// let panesString: string = `
+//   {
+//     "ghd": {
+//       "datasource": "${datasource}",
+//       "queries": [
+//         {
+//           "refId": "A",
+//           "expr": "${expr}",
+//           "queryType": "range",
+//           "datasource": { "type": "loki", "uid": "${datasource}" },
+//           "editorMode": "builder"
+//         }
+//       ],
+//       "range": { "from": "${getCreateTimeSinceEpoch}", "to": "${getLastUpdateTimeSinceEpoch}" }
+//     }
+//   }
+// `;
+// let panesJson: JSON = JSON.parse(panesString);
+// let panes: string = JSON.stringify(panesJson);
+// console.log(panes)
+// const grafanaUrl = new URL(url);
+// grafanaUrl.searchParams.append("schemaVersion", "1");
+// grafanaUrl.searchParams.append("panes", panes);
+// grafanaUrl.searchParams.append("orgId", "1");
+// console.log(grafanaUrl);
+// console.log(grafanaUrl.toString());
--- typescript/helo_react.tsx
+++ typescript/helo_react.tsx
@@ -3,34 +3,60 @@ let podName: string = "pipeline-4vqqd-3932826485";
 let getCreateTimeSinceEpoch: number = 1721149200000;
 let getLastUpdateTimeSinceEpoch: number = 1721408399000;
 
-let url: string = "https://grafana.mon.tipnet.xyz/explore";
-let datasource: string = "loki-core-staging-79yu";
-let expr: string = `{namespace=\\"${podNameSpace}\\",pod=\\"${podName}\\"}|=\`\``;
+var date = new Date(getCreateTimeSinceEpoch);
+console.log(date);
+var hours = date.getHours();
+console.log(hours);
+var minutes = date.getMinutes();
+console.log(minutes);
+var day = date.getDate();
+console.log(day);
+var time = date.getTime();
+console.log(time);
 
-let panesString: string = `
-  {
-    "ghd": {
-      "datasource": "${datasource}",
-      "queries": [
-        {
-          "refId": "A",
-          "expr": "${expr}",
-          "queryType": "range",
-          "datasource": { "type": "loki", "uid": "${datasource}" },
-          "editorMode": "builder"
-        }
-      ],
-      "range": { "from": "${getCreateTimeSinceEpoch}", "to": "${getLastUpdateTimeSinceEpoch}" }
-    }
-  }
-`;
-let panesJson: JSON = JSON.parse(panesString);
-let panes: string = JSON.stringify(panesJson);
-console.log(panes)
+var date_kurang = new Date(getCreateTimeSinceEpoch);
+date_kurang.setHours(date_kurang.getHours() - 2);
+var hours1 = date_kurang.getHours();
+console.log(hours1);
+var day1 = date_kurang.getDate();
+console.log(day1);
 
-const grafanaUrl = new URL(url);
-grafanaUrl.searchParams.append("schemaVersion", "1");
-grafanaUrl.searchParams.append("panes", panes);
-grafanaUrl.searchParams.append("orgId", "1");
-console.log(grafanaUrl);
-console.log(grafanaUrl.toString());
+console.log("lebih");
+var date_lebih = new Date(getCreateTimeSinceEpoch);
+date_lebih.setHours(date_lebih.getHours() + 2);
+var hours2 = date_lebih.getHours();
+console.log(hours2);
+var day2 = date_lebih.getDate();
+console.log(day2);
+
+// let url: string = "https://grafana.mon.tipnet.xyz/explore";
+// let datasource: string = "loki-core-staging-79yu";
+// let expr: string = `{namespace=\\"${podNameSpace}\\",pod=\\"${podName}\\"}|=\`\``;
+
+// let panesString: string = `
+//   {
+//     "ghd": {
+//       "datasource": "${datasource}",
+//       "queries": [
+//         {
+//           "refId": "A",
+//           "expr": "${expr}",
+//           "queryType": "range",
+//           "datasource": { "type": "loki", "uid": "${datasource}" },
+//           "editorMode": "builder"
+//         }
+//       ],
+//       "range": { "from": "${getCreateTimeSinceEpoch}", "to": "${getLastUpdateTimeSinceEpoch}" }
+//     }
+//   }
+// `;
+// let panesJson: JSON = JSON.parse(panesString);
+// let panes: string = JSON.stringify(panesJson);
+// console.log(panes)
+
+// const grafanaUrl = new URL(url);
+// grafanaUrl.searchParams.append("schemaVersion", "1");
+// grafanaUrl.searchParams.append("panes", panes);
+// grafanaUrl.searchParams.append("orgId", "1");
+// console.log(grafanaUrl);
+// console.log(grafanaUrl.toString());
