19c46ee51ccb3e07bdde9e5820c96ac8b1c3ab2c
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2024-08-31 15:38:46 +0700

    Add python/scrape_graph_ai.py

---
+++ python/scrape_graph_ai.py
@@ -0,0 +1,25 @@
+import json
+import os
+
+from scrapegraphai.graphs import SmartScraperGraph
+
+# Define the configuration for the scraping pipeline
+graph_config = {
+    "llm": {
+        "api_key": os.environ["OPENAI_APIKEY"],
+        "model": "openai/gpt-4o-mini",
+    },
+    "verbose": True,
+    "headless": False,
+}
+
+# Create the SmartScraperGraph instance
+smart_scraper_graph = SmartScraperGraph(
+    prompt="Find some information about what does the company do, the name and a contact email.",  # noqa: E501
+    source="https://scrapegraphai.com/",
+    config=graph_config,
+)
+
+# Run the pipeline
+result = smart_scraper_graph.run()
+print(json.dumps(result, indent=4))
