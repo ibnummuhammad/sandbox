9863f13bed2a0c5b91cf480c373730270ae8e046
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2024-10-31 22:11:58 +0700

    Add monster.pdf

--- llm_rag/rag_llm.py
+++ llm_rag/rag_llm.py
@@ -1,8 +1,12 @@
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain_community.document_loaders import PDFPlumberLoader
-# from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_ollama.llms import OllamaLLM
 from langchain_community.vectorstores import FAISS
 from langchain_experimental.text_splitter import SemanticChunker
+from langchain.chains import RetrievalQA
+from langchain.chains.llm import LLMChain
+from langchain.chains.combine_documents.stuff import StuffDocumentsChain
+from langchain.prompts import PromptTemplate
 
 
 loader = PDFPlumberLoader("monster.pdf")
@@ -23,4 +27,42 @@ vector = FAISS.from_documents(documents, embedder)
 
 retriever = vector.as_retriever(search_type="similarity", search_kwargs={"k": 3})
 retrieved_docs = retriever.invoke("Who is naoki urasawa?")
-print(retrieved_docs)
+
+llm = OllamaLLM(model="mistral")
+print(llm)
+
+prompt = """
+1. Use the following pieces of context to answer the question at the end.
+2. If you don't know the answer, just say that "I don't know" but don't make up an answer on your own.\n
+3. Keep the answer crisp and limited to 3,4 sentences.
+
+Context: {context}
+
+Question: {question}
+
+Helpful Answer:"""
+
+QA_CHAIN_PROMPT = PromptTemplate.from_template(prompt)
+
+llm_chain = LLMChain(llm=llm, prompt=QA_CHAIN_PROMPT, callbacks=None, verbose=True)
+
+document_prompt = PromptTemplate(
+    input_variables=["page_content", "source"],
+    template="Context:\ncontent:{page_content}\nsource:{source}",
+)
+
+combine_documents_chain = StuffDocumentsChain(
+    llm_chain=llm_chain,
+    document_variable_name="context",
+    document_prompt=document_prompt,
+    callbacks=None,
+)
+
+qa = RetrievalQA(
+    combine_documents_chain=combine_documents_chain,
+    verbose=True,
+    retriever=retriever,
+    return_source_documents=True,
+)
+
+print(qa("Who is naoki urasawa?")["result"])
